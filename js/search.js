6c1c8cc586cd224655d853cef5dc536c302b6c8a
Alex Hart <w.alexhart@gmail.com>
2018-03-07 17:43:30 +0000

    Refactor code

--- js/search.js
+++ js/search.js
@@ -6,10 +6,13 @@ $(document).ready(function() {
     //Storing user's search as a variable
     var searchQuery = $("#search-bar").val();
     //Accessing the Wikipedia API based on user input
-    $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&datatype=json&limit=5&search=" + searchQuery + "&callback=?", function(data) {
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&datatype=json&limit=5&search="
+    + searchQuery + "&callback=?", function(data) {
       //Updating HTML of webpage for every search result
       for (i = 0; i < data[1].length; i++) {
-        html += "<a class='article-links' href='" + data[3][i] + "' target='blank'> <div class='article-result'> <h1>" + data[1][i] + "</h1> <p>" + data[2][i] + "</p> </div> </a>"
+        html += "<a class='article-links' href='" + data[3][i] +
+        "' target='blank'> <div class='article-result'> <h1>" + data[1][i] +
+        "</h1> <p>" + data[2][i] + "</p> </div> </a>"
       }
       $(".search-results").html(html);
       $(".article-result").hide();
